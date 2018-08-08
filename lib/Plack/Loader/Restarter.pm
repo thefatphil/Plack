79b1f76c8b24d6c01e9e3ece5808269799af15f2
Phil Carmody <pc+dev@asdf.org>
2018-08-08 17:54:13 +0300

    Restarter - fix vim's bizarre numeric temporaries
    The files are 4913, 5036, ... spaced out by 123, not 4193..5036.
    
    Signed-off-by: Phil Carmody <pc+dev@asdf.org>

--- lib/Plack/Loader/Restarter.pm
+++ lib/Plack/Loader/Restarter.pm
@@ -47,9 +47,9 @@ sub _kill_child {
 sub valid_file {
     my($self, $file) = @_;
 
-    # vim temporary file is  4913 to 5036
+    # vim temporary file is 4913, 5036, ...
     # http://www.mail-archive.com/vim_dev@googlegroups.com/msg07518.html
-    if ( $file->{path} =~ m{(\d+)$} && $1 >= 4913 && $1 <= 5036) {
+    if ( $file->{path} =~ m{(\d+)$} && $1 >= 4913 && ($1-4913)%123 == 0) {
         return 0;
     }
     $file->{path} !~ m!\.(?:git|svn)[/\\]|\.(?:bak|swp|swpx|swx)$|~$|_flymake\.p[lm]$|\.#!;
